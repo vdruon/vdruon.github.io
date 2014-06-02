0625942355a3080407347a6fcb6f5f0290fa5062
Valentin Druon <valentin@valentin.home>
2014-06-02 16:44:49 +0200

    Adding parallax effect to ruby

--- resources/js/global.js
+++ resources/js/global.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   $.localScroll();
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    $("#ruby-scroll").css("-webkit-transform", "translateY(" + scroll/4 + "px)");
+    $("#ruby-scroll").css("-moz-transform", "translateY(" + scroll/4 + "px)");
+    $("#ruby-scroll").css("-o-transform", "translateY(" + scroll/4 + "px)");
+  });
 });
