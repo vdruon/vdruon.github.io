711f9b76ebacd63497557386c3996cbee0b8a459
Valentin Druon <valentin.druon@gmail.com>
2014-10-21 20:10:15 +0200

    random'

--- constance/resources/js/global.js
+++ constance/resources/js/global.js
@@ -3,3 +3,12 @@ $.stellar({
             scrollProperty: 'scroll',
             positionProperty: 'position'
         });
+
+$(document).ready(function() {
+  var ids = ["#romeo-1", "#romeo-2", "#romeo-3", "#english-text"];
+
+  var index = Math.floor((Math.random() * 3) + 1);
+  var element = $(ids[index]);
+
+  element.css('display', 'block');
+});
