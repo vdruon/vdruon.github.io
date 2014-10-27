3b3a904ef7e49d117e9f6bbfe6f49c020a6b660d
Valentin Druon <valentin.druon@gmail.com>
2014-10-27 16:57:26 +0100

    Mail

---
+++ constance/resources/js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+  function sendMail(text) {
+    $.ajax({
+      type: "POST",
+      url: "https://mandrillapp.com/api/1.0/messages/send.json",
+      data: {
+        'key': 'Ld4UoxMJBAqYJ9x9lc3qfg',
+        'message': {
+          'from_email': 'valentin.druon@gmail.com',
+          'to': [
+              {
+                'email': 'valentin.druon@gmail.com',
+                'name': 'Valentin DRUON',
+                'type': 'to'
+              }
+            ],
+          'autotext': 'true',
+          'subject': 'Mail from Constance',
+          'html': 'Constance dit : ' + text
+        }
+      }
+     }).done(function(response) {
+        $(".button-group").hide();
+     });
+  }
+
+  $("#btn-love").on('click', function() {
+    sendMail("Je t'aime.");
+  });
+
+  $("#btn-hate").on('click', function() {
+    sendMail("Je te d&eacute;teste.");
+  });
+
+  $("#btn-come").on('click', function() {
+    sendMail("Reviens.");
+  });
+
+  $("#btn-go").on('click', function() {
+    sendMail("D&eacute;gage.");
+  });
+
+});
